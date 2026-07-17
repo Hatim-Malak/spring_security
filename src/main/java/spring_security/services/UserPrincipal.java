63792cba35aebfea2c5f30dc7479a318eb7575fe
Hatim Malak <hatim05042006@gmail.com>
2026-07-17 11:24:48 +0530

    Fix import statement for UserDetails in UserPrincipal and change return type of verify method in UserService from int to String

--- src/main/java/spring_security/services/UserPrincipal.java
+++ src/main/java/spring_security/services/UserPrincipal.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import spring_security.model.User;
 
